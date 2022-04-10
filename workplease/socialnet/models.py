ac4426dcc3efb70df4e64beea9ea9b4cf130f13a
evgeny <liejkee@yandex.ru>
2022-04-10 22:23:33 +0300

    added user page edit function

--- workplease/socialnet/models.py
+++ workplease/socialnet/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class UserInfo(models.Model):
-    first_name = models.SlugField(max_length=255)
-    last_name = models.SlugField(max_length=255)
-    city = models.SlugField(max_length=255)
-    custom_url = models.SlugField(max_length=255)
-    nationality = models.SlugField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    custom_url = models.CharField(max_length=255)
+    nationality = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     is_private = models.BooleanField(default=True)
     create_time = models.DateTimeField(auto_now_add=True)
     date_of_birth = models.DateTimeField(auto_now_add=True)
+
--- workplease/socialnet/views.py
+++ workplease/socialnet/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from .forms import *
 from .models import *
+from django.contrib import messages
 
 menu = [{'title': 'My page', 'url_name': 'user_page'},
         {'title': 'My friends', 'url_name': 'friends'},
@@ -11,7 +12,7 @@ menu = [{'title': 'My page', 'url_name': 'user_page'},
 
 
 def user_page(request):
-    app_elements = UserInfo.objects.all()
+    app_elements = UserInfo.objects.all()[:1]
     return render(request, 'socialnet/user_page.html', {'app_elements': app_elements, 'menu': menu,
                                                         'title': 'My page'})
 
@@ -20,32 +21,34 @@ def friends(request):
     return render(request, 'socialnet/friends_page.html', {'menu': menu, 'title': 'My friends'})
 
 
-def settings(request):
-    form = UserForm()
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('settings_page')
-        else:
-            form = UserForm()
-    return render(request, 'socialnet/settings_page.html', {'form': form, 'menu': menu, 'title': 'Settings'})
+# def settings(request):
+#     form = UserForm()
+#     if request.method == 'POST':
+#         form = UserForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('settings')
+#     return render(request, 'socialnet/settings_page.html', {'form': form, 'menu': menu, 'title': 'Settings'})
 
 
 def login(request):
     return render(request, 'socialnet/login_page.html', {'menu': menu, 'title': 'Login'})
 
 
-def clean_first_name(self):
-    first_name = self.clened_data['first_name']
-    if len(first_name) > 5:
-        raise ValidationError('Error')
-    return first_name
-
-
-def pageNotFound(request, exception):
-    return HttpResponseNotFound('Страница не найдена')
-
-
 def view_404(request, exception=None):
     return redirect('user_page', permanent=False)
+
+
+def settings(request):
+    app_elements = UserInfo.objects.get(id=1)
+    form = UserForm()
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=app_elements)
+        if form.is_valid():
+            messages.success(request, 'User data has changed successfully')
+            form.save()
+            return redirect('settings')
+
+    return render(request, 'socialnet/settings_page.html', {'form': form, 'menu': menu,
+                                                            'title': 'Settings'})
